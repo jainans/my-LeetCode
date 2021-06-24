5bfcbc3ad5d96a118bd2127d4454f0455a0c86cc
Anshuman Jain <72242262+jainans@users.noreply.github.com>
2021-06-24 15:27:09 +0530

    Time: 4 ms (90.61%), Space: 7.9 MB (64.11%) - LeetHub

---
+++ counting-bits/counting-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>ans(n+1);
+        ans[0]=0;
+        for(int i=1; i<=n; i++){
+            ans[i]=(ans[i/2]);
+            if(i%2==1)
+                ans[i]++;
+        }
+        return ans;
+    }
+};
