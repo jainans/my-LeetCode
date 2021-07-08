549958e2881514fd62bfe17a02401dbee2562036
Anshuman Jain <72242262+jainans@users.noreply.github.com>
2021-07-08 18:21:28 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (37.88%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    void dfs(int st, string dg, int n, string temp, vector<string>& given, vector<string>& ans){
-        if(st==n){
-            ans.push_back(temp);
-            return;
-        }
-        int p = dg[st]-'0';
-        for(int i=0; i<given[p].size(); i++){
-            temp.push_back(given[p][i]);
-            dfs(st+1, dg, n, temp, given, ans);
-            temp.pop_back();
-        }
-    }
+   
     vector<string> letterCombinations(string digits) {
         vector<string>given=  {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         vector<string>ans;
-        string temp = "";
+        
         int n = digits.size();
         if(n==0)
             return ans;
-        dfs(0, digits, n, temp, given, ans);
+        string temp = "";
+        ans.push_back(temp);
+        for(auto digit : digits){
+            vector<string>temp;
+            for(auto s : ans){
+            for(auto c : given[digit-'0']){
+                temp.push_back(s+c);
+            }
+            }
+            ans = temp;
+        }
         return ans;
     }
 };
