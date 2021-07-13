86acd6924fe15fea414ec4e163410214956e5360
Anshuman Jain <72242262+jainans@users.noreply.github.com>
2021-07-13 18:19:45 +0530

    Time: 12 ms (94.10%), Space: 15.8 MB (66.61%) - LeetHub

--- path-sum-iii/path-sum-iii.cpp
+++ path-sum-iii/path-sum-iii.cpp
@@ -1,52 +1,13 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    map<pair<TreeNode*, int> , int >mpnot;
-    map<pair<TreeNode*, int> , int >mpyes;
-    int solve(TreeNode* root, int trg, int flag){
-        if(!root)
-            return 0;
-        if(root->left==NULL && root->right==NULL){
-            if(flag==1){
-                if(root->val==trg)
-                    return mpyes[{root, trg}]=1;
-                else
-                    return mpyes[{root, trg}]=0;
-            }
-            else{
-                return mpnot[{root, trg}]=0;
-            }
-        }
-        if(flag==1){
-            if(mpyes.find({root, trg})!=mpyes.end())
-                return mpyes[{root, trg}];
-            mpyes[{root, trg}]=0;
-            mpyes[{root, trg}]+=solve(root->left, trg-root->val, 1)+solve(root->right, trg-root->val, 1);
-            if(root->val==trg)
-                mpyes[{root, trg}]+=1;
-            return mpyes[{root, trg}];
-        }
-        else{
-            if(mpnot.find({root, trg})!=mpnot.end())
-                return mpnot[{root, trg}];
-            mpnot[{root, trg}]=0;
-            mpnot[{root, trg}]+=solve(root->left, trg, 1)+solve(root->left, trg, 0)+solve(root->right, trg, 1)+solve(root->right, trg, 0);
-            return mpnot[{root, trg}];
-        }
-            
+    int pathSum(TreeNode* root, int sum) {
+        if(!root) return 0;
+        return sumUp(root, 0, sum) + pathSum(root->left, sum) + pathSum(root->right, sum);
     }
-    int pathSum(TreeNode* root, int targetSum) {
-       int ans = solve(root, targetSum, 0)+solve(root, targetSum, 1);
-        return ans;
+private:
+    int sumUp(TreeNode* root, int pre, int& sum){
+        if(!root) return 0;
+        int current = pre + root->val;
+        return (current == sum) + sumUp(root->left, current, sum) + sumUp(root->right, current, sum);
     }
 };
