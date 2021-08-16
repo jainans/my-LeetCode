76ef7e57231fb28e6da612fad017dc5ca0b4ea99
Anshuman Jain <72242262+jainans@users.noreply.github.com>
2021-08-16 08:51:31 +0530

    Time: 452 ms (93.65%), Space: 139.6 MB (55.08%) - LeetHub

--- remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,65 +1,53 @@
+int find(int x, vector<int>& par){
+    if(par[x]==x)
+        return x;
+    return par[x] = find(par[x], par);
+}
+void join(int x, int y, vector<int>& par){
+    x= find(x, par);
+    y=  find(y, par);
+    if(x!=y){
+        par[x]=y;
+    }
+}
 class Solution {
 public:
-
-    int alice[100001];
-    int bob[100001];
-  
-int finda(int a){
-	if(alice[a]==-1)
-	return a;
-	return alice[a]=finda(alice[a]);
-}
-    int findb(int a){
-	if(bob[a]==-1)
-	return a;
-	return bob[a]=findb(bob[a]);
-}
-void mergea (int a,int b){
-	alice[a]=b;
-}
-    void mergeb (int a,int b){
-	bob[a]=b;
-}
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        for(int i=0;i<100001;i++){
-	alice[i]=-1;
-            bob[i]=-1;
-}   
-     int t3=0;//essential edges for both 
-        for(int i=0;i<edges.size();i++){
+        vector<int>par1(n+1);
+        vector<int>par2(n+1);
+        for(int i=0; i<=n; i++){
+            par1[i]=i;
+            par2[i]=i;
+        }
+        int t=0;
+        for(int i=0; i<edges.size(); i++){
             if(edges[i][0]==3){
-                if(finda(edges[i][1])!=finda(edges[i][2])){
-                   mergea(finda(edges[i][1]),finda(edges[i][2]));
-                   mergeb(findb(edges[i][1]),findb(edges[i][2]));
-                    t3++;
-                }
+                if(find(edges[i][1], par1)!=find(edges[i][2], par1)){
+                    join(edges[i][1], edges[i][2], par1);
+                    join(edges[i][1], edges[i][2], par2);
+                    t++;
+                    }
             }
         }
-        int t1=0; // essential edges for alice
-         for(int i=0;i<edges.size();i++){
+        for(int i=0; i<edges.size(); i++){
             if(edges[i][0]==1){
-                if(finda(edges[i][1])!=finda(edges[i][2])){
-                    mergea(finda(edges[i][1]),finda(edges[i][2]));
-                    
-                    t1++;
-                   
-                }
+                if(find(edges[i][1], par1)!=find(edges[i][2], par1)){
+                    join(edges[i][1], edges[i][2], par1);
+                    t++;
+                    }
+            }
+            else if(edges[i][0]==2){
+                if(find(edges[i][1], par2)!=find(edges[i][2], par2)){
+                    join(edges[i][1], edges[i][2], par2);
+                    t++;
+                    }
             }
         }
-        int t2=0; //essential edges for bob
-         for(int i=0;i<edges.size();i++){
-            if(edges[i][0]==2){
-                if(findb(edges[i][1])!=findb(edges[i][2])){
-                    mergeb(findb(edges[i][1]),findb(edges[i][2]));
-                    
-                    t2++;
-                }
-            }
+        for(int i=1; i<=n; i++){
+            if(find(i, par1)!=find(1, par1) || find(i, par2)!=find(1, par2))
+                return -1;
+            
         }
-   if(t1+t3<n-1||t3+t2<n-1)// if(t1+t3<n-1) then alice can not traverse the graph because tree requires n-1 edges to be spanned completely if(t2+t3<n-1)then bob can not traverse.
-       return -1;
-        
-        cout<<t1<<" "<<t2<<" "<<t3<<endl;
-      return edges.size()-t1-t2-t3;  //(t1+t2+t3) are essential edges
+        return edges.size()-t;
     }
 };
