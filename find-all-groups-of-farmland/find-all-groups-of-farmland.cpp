f5e640e7cbd988dbe528ff3c7c11e517d8b463ec
Anshuman Jain <72242262+jainans@users.noreply.github.com>
2021-09-04 22:03:15 +0530

    Time: 160 ms (72.73%), Space: 68.4 MB (72.73%) - LeetHub

--- find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
+++ find-all-groups-of-farmland/find-all-groups-of-farmland.cpp
@@ -1,36 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> nbrs = {{0,1},{1,0},{-1,0},{0,-1}};
-    pair<int, int> dfs(vector<vector<int>> &land, int i, int j, vector<vector<bool>> &visited) {
-        visited[i][j] = true;
-        pair<int, int> res  = make_pair(i, j);
-        for(auto &nbr: nbrs) {
-            int x = i + nbr[0];
-            int y = j + nbr[1];
-            if(x < 0 || y < 0 || x >= land.size() || y >= land[0].size() || visited[x][y] || land[x][y] != 1)
-                continue;
-            pair<int, int> ans = dfs(land, x, y, visited);
-            res.first = max(res.first, ans.first);
-            res.second = max(res.second, ans.second);
-        }
-        return res;
-    }
     vector<vector<int>> findFarmland(vector<vector<int>>& land) {
         int m = land.size();
         int n = land[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        vector<vector<int>> ans;
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(!visited[i][j] && land[i][j] == 1) {
-                    pair<int, int> p = dfs(land, i, j, visited);
-                    vector<int> res;
-                    res.push_back(i);
-                    res.push_back(j);
-                    res.push_back(p.first);
-                    res.push_back(p.second);
-                    ans.push_back(res);
-                    cout << 1 << endl;
+        vector<vector<int> >ans;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(land[i][j]==1 && (j==0 || land[i][j-1]==0) && (i==0 || land[i-1][j]==0)){
+                    vector<int>temp;
+                    temp.push_back(i);
+                    temp.push_back(j);
+                    int k=j;
+                    while(k<n && land[i][k]==1)
+                        k++;
+                    k--;
+                    int l=i;
+                    while(l<m && land[l][j]==1)
+                        l++;
+                    l--;
+                    temp.push_back(l);
+                    temp.push_back(k);
+                    ans.push_back(temp);
                 }
             }
         }
